4b58f6afbec5ceb8f4d68c3688a3a6a11449975c
yousufzai8454 <yousufzai8454@gmail.com>
2022-05-17 19:17:02 -0400

    Solar System basic project ready

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,16 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        	// write your code here
+
+        SolarSystem FactorX=new SolarSystem();
+        FactorX.Sun="1";
+        FactorX.Moon="4";
+        FactorX.Planet="2";
+        FactorX.Star="10000";
+    }
+}
+    }
+}
---
+++ src/com/syntax/SolarSystem.java
@@ -0,0 +1,12 @@
+package com.syntax;
+
+public class SolarSystem {
+    public class SolarSystem {
+
+        String Sun;
+        String Moon;
+        String Star;
+        String Planet;
+
+    }
+}
